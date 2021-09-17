717e74ed3c144119f96cea79a3642b77c95e261d
Vignesh-02 <laserlikefocus000@gmail.com>
2021-09-17 20:47:37 +0530

    delete message changed

--- routes/comments.js
+++ routes/comments.js
@@ -73,7 +73,7 @@ router.delete("/:comment_id",middleware.checkCommentOwnership,function(req,res){
         if(err){
             res.redirect("back");
         }else{
-            req.flash("success","Comment not deleted");
+            req.flash("success","Comment deleted");
             res.redirect("/campgrounds/"+req.params.id);
         }        
     });
